dd79ac27dfd10528c3e0e687626bb9210d9af1c5
unknown <reinelr13@gmail.com>
2023-02-12 21:52:09 -0500

    se añade diseño respinsive

--- src/componentes/BarraLateral.jsx
+++ src/componentes/BarraLateral.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BarraLateral = () => {
   return (
     <div className='BarraLateral'>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt dolor iusto voluptates cumque ipsam aliquam, quae vel rerum voluptas inventore fuga tempora optio tempore distinctio reiciendis at maiores. Voluptate, adipisci.</p>
+      <p></p>
     </div>
   )
 }
--- src/componentes/Experiencia.jsx
+++ src/componentes/Experiencia.jsx
@@ -5,15 +5,21 @@ const Experiencia = () => {
     <div className='Experiencia'>
       <h3>Experiencia</h3>
       <h5>Desarrollo de Proyectos </h5>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, provident. Minima placeat est obcaecati aliquid illum magni veritatis sed quis aspernatur, nam vero deserunt, id eos, facere culpa molestias odit.</p>
+      <p>Cuento con experiencia desarrollando proyectos independientes 
+        utilizando las principales tecnologias web, html, css y javascript.
+        Para obtimizar la produccion de estos proyectos utlizo una de las principales
+        librerias de javascript como lo es react, con esta puedo desarrollar aplicaciones 
+        web basadas en componentes, implenter el manejo del estado de la aplicacion 
+        utilizando el hook useState 
+      </p>
 
       <ul>
         <li>Proyectos de React</li>
         <li>Proyectos de React</li>
         <li>Proyectos de React</li>
       </ul>
-      <h5>Desarrollo de Actividades </h5>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae exercitationem, non voluptas est deleniti aspernatur laboriosam reprehenderit, repellat excepturi sequi porro obcaecati, voluptates eveniet adipisci ratione nisi minima accusamus nihil.</p>
+     
+      
       </div>
   )
 }
